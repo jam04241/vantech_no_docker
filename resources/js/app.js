924ca875462f8491747127061ec4bd3ea3a3203a
Jam04241 <akog486@gmail.com>
2025-12-03 21:41:32 +0800

    Added: package for htmx, sweetalert, fontawesome, chart.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,10 @@
 import './bootstrap';
 import '../css/app.css';
+import htmx from 'htmx.org';
+import Swal from 'sweetalert2';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Chart from 'chart.js/auto';
+
+window.htmx = htmx;
+window.Swal = Swal;
+window.Chart = Chart;
