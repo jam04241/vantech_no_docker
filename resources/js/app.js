b71f38bc529c21dc5f47056be76c2fbbbc5286df
Jam04241 <akog486@gmail.com>
2025-12-14 01:36:00 +0800

    Update: add local bootstrap icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import '../css/app.css';
 import htmx from 'htmx.org';
 import Swal from 'sweetalert2';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import Chart from 'chart.js/auto';
 
 window.htmx = htmx;
